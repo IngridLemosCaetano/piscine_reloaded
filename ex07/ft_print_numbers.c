297e65f6eadadd7645d74279f9c08eb83dd37c39
IngridLemos <ingridlc42@gmail.com>
2025-07-15 11:40:04 -0300

    ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 18:54:09 by ilemos-c          #+#    #+#             */
+/*   Updated: 2025/07/12 11:49:02 by ilemos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	int	num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		ft_putchar(num);
+		num++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}*/
