1c822e6e06d7ce4aad813f3f934d909bd077b1de
IngridLemos <ingridlc42@gmail.com>
2025-07-15 11:44:02 -0300

    ex16

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 12:35:37 by ilemos-c          #+#    #+#             */
+/*   Updated: 2025/07/10 16:51:51 by ilemos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+int	main(void)
+{
+	char	*str = "Ingrid";
+	printf("Lenght de %s: %d", str,ft_strlen(str));
+	return(0);
+}*/
