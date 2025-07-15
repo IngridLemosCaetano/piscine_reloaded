3ccf7828bbcacae87843cce178efd19a7c9893ad
IngridLemos <ingridlc42@gmail.com>
2025-07-15 11:39:44 -0300

    ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 11:37:31 by ilemos-c          #+#    #+#             */
+/*   Updated: 2025/07/12 11:58:52 by ilemos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
