c7110e803f6d52872e2f0860eabef1f29a9de55b
IngridLemos <ingridlc42@gmail.com>
2025-07-15 11:43:36 -0300

    ex15

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 12:25:49 by ilemos-c          #+#    #+#             */
+/*   Updated: 2025/07/12 11:49:48 by ilemos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char	*string = "Ingrid";
+	ft_putstr(string);
+}*/
